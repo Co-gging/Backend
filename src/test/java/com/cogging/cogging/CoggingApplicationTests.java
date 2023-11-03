1437bd9de4373101eba577e6ba192e541f72c4e1
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-03 23:44:08 +0900

    feat: setup project

---
+++ src/test/java/com/cogging/cogging/CoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogging.cogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
