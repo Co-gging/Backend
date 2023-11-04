a0f087907c21ee6ca82aea680655dba1355eeda4
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-11-04 21:24:12 +0900

    feat: 커뮤니티 글 생성을 위한 함수 추가

---
+++ src/main/java/com/cogging/cogging/service/CommunityService.java
@@ -0,0 +1,37 @@
+package com.cogging.cogging.service;
+
+import com.cogging.cogging.dto.CommunityDto;
+import com.cogging.cogging.entity.CommunityEntity;
+import com.cogging.cogging.entity.UserEntity;
+import com.cogging.cogging.repository.CommunityRepository;
+import com.cogging.cogging.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityService {
+
+    private final UserRepository userRepository;
+    private final CommunityRepository communityRepository;
+
+    @Transactional
+    public void createCommunity(Map<String, String> param){
+
+        UserEntity user = userRepository.findById(Integer.parseInt(param.get("id")))
+                .orElseThrow(() -> new RuntimeException());
+
+        LocalDateTime date = LocalDateTime.parse(param.get("date"), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        CommunityDto communityDto = new CommunityDto(param.get("title"), param.get("content"), date, 0);
+
+        communityDto.setUser(user);
+
+        CommunityEntity communityEntity = communityDto.toEntity();
+        communityRepository.save(communityEntity);
+    }
+}
