83a649f11a49588a678440bd9d0ddd170ca8b4bc
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-06 00:20:06 +0900

    feat: exception 처리

---
+++ src/main/java/com/cogging/cogging/exceptions/BaseException.java
@@ -0,0 +1,27 @@
+package com.cogging.cogging.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class BaseException extends RuntimeException {
+    protected HttpStatus status;
+    protected Date thrownAt;
+
+    public BaseException(String message, Throwable cause, HttpStatus status) {
+        super(message, cause);
+
+        this.status = status;
+        if (status == null) {
+            this.status = HttpStatus.CONFLICT;
+        }
+
+        this.thrownAt = new Date();
+    }
+}
