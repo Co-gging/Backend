6a86150ce384ef2d240b18623285a9cb5d0e1093
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-11-05 14:05:16 +0900

    feat: community id로 탐색하는 코드 추가

--- src/main/java/com/cogging/cogging/repository/CommunityRepository.java
+++ src/main/java/com/cogging/cogging/repository/CommunityRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommunityRepository extends JpaRepository<CommunityEntity, Integer> {
 
     List<CommunityEntity> findAll();
+
+    Optional<CommunityEntity> findById(Integer id);
 }
