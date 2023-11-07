2db4fe47be0c2b0713dc4c91653c6d4d0e5a32c3
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-08 00:14:32 +0900

    feat: community repository, service, controller 생성

---
+++ src/main/java/com/cogging/cogging/service/CommunityService.java
@@ -0,0 +1,9 @@
+package com.cogging.cogging.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityService {
+}
