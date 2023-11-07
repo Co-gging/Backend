ce2d2460cfdaee9b07bcc77a99fba6b1b4944e4d
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-08 01:24:04 +0900

    refactor: 상수명 수정

--- src/main/java/com/cogging/cogging/exceptions/ExceptionConstants.java
+++ src/main/java/com/cogging/cogging/exceptions/ExceptionConstants.java
@@ -1,5 +1,5 @@
 package com.cogging.cogging.exceptions;
 
 public class ExceptionConstants {
-    public static String CANNOT_FIND_USER = "사용자를 찾을 수 없습니다.";
+    public static String USER_NOT_FOUND = "사용자를 찾을 수 없습니다.";
 }
