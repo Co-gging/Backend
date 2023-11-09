0aa7ca4b5b2c0e9280c1b24c750df0b189584122
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-09 21:00:45 +0900

    fix: MemberRepository JPA repository 자료형 수정

--- src/main/java/com/cogging/cogging/repository/MemberRepository.java
+++ src/main/java/com/cogging/cogging/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import org.springframework.lang.NonNullApi;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findById(int id);
     Optional<Member> findByEmail(String email);
     Optional<Member> findByNickname(String nickname);
