7012aa0b9b8e630e7e30d07268d1c4097e9ce9ab
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-11 22:35:53 +0900

    feat: review 엔티티 생성

---
+++ src/main/java/com/cogging/cogging/entity/Review.java
@@ -0,0 +1,36 @@
+package com.cogging.cogging.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "review")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
